238f9f8aba08707469d96dc7309b72056aa1cbe6
optospinlab <optospinlab@gmail.com>
2015-07-29 16:24:57 -0700

    New Centroid Fixed.

--- DC2/diamondControl.m
+++ DC2/diamondControl.m
@@ -7,7 +7,7 @@
 %    collecting from the same spot,
 %  - control of the peizo stage for precise Z and XY positioning,
 %  - poorly-implemented optimization routines, and
-%  - (soon [edit: now]) basic automation protocols for preforming simple testing.
+%  - (soon [edit: now]) basic automation protocols for performing simple testing.
 function varargout = diamondControl(varargin)
     if isempty(varargin)    % If no variables have been given, make the figure
         f = figure('Visible', 'off', 'tag', 'Diamond Control', 'Name', 'Diamond Control', 'Toolbar', 'figure', 'Menubar', 'none');
@@ -962,24 +962,35 @@ function varargout = diamondControl(varargin)
     end
     function [x, y] = myMean(data, X, Y)
         % New Method
-%         [labels, ~] = bwlabel(data, 4);
-%         measurements = regionprops(labels, 'Area', 'Centroid');
-%         areas = [measurements.Area];
-%         [~, indexes] = sort(areas, 'descend');
-%         
-%         centroid = measurements(indexes(1)).Centroid;
-%         
-%         x = X(centroid(1));
-%         y = Y(centroid(2));
-%         
-%         plot(c.upperAxes, X, Y, labels(indexes(1)));
-        
+        dim = size(data);
+    
+        data = imdilate(data, strel('diamond', 1));
+
+        [labels, ~] = bwlabel(data, 8);
+        measurements = regionprops(labels, 'Area', 'Centroid');
+        areas = [measurements.Area];
+        [~, indexes] = sort(areas, 'descend');
+
+        centroid = measurements(indexes(1)).Centroid;
+
+        x = linInterp(1, min(X), dim(2), max(X), centroid(1));
+        y = linInterp(1, min(Y), dim(1), max(Y), centroid(2));
+    
         % Old Method
         % Calculates the centroid.
-        total = sum(sum(data));
-        dim = size(data);
-        x = sum(data*((X((length(X)-dim(2)+1):end))'))/total;
-        y = sum((Y((length(Y)-dim(1)+1):end))*data)/total;
+%         total = sum(sum(data));
+%         dim = size(data);
+%         x = sum(data*((X((length(X)-dim(2)+1):end))'))/total;
+%         y = sum((Y((length(Y)-dim(1)+1):end))*data)/total;
+    end
+    function y = linInterp(x1, y1, x2, y2, x)    % Perhaps make it a spline in the future...
+        if x1 < x2
+            y = ((y2 - y1)/(x2 - x1))*(x - x1) + y1;
+        elseif x1 > x2
+            y = ((y1 - y2)/(x1 - x2))*(x - x2) + y2;
+        else
+            y = (y1 + y2)/2;
+        end
     end
 %     function data = displayImage()
 %         start(c.vid);
