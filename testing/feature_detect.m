8bda30fdabf67d38d28a19970d002944ae3c298b
optospinlab <optospinlab@gmail.com>
2015-10-20 14:46:25 -0700

    Merge2

--- testing/feature_detect.m
+++ testing/feature_detect.m
@@ -1,26 +1,44 @@
 close all;
 
-
-I1 = img_enhance(rgb2gray(imread('test_image.png')));
-I2 = img_enhance(rgb2gray(imread('test_image_shifted1.png')));
+I1 = imadjust(rgb2gray(imread('t3.png')));
+I2 = imadjust(rgb2gray(imread('t4.png')));
 
 tic
-roi1 = I1(20:220,20:300);
-C1 = corner(roi1,50);
-
-roi2 = I2(20:220,20:300);
-C2 = corner(roi2,50);
-
-[features1, valid_points1] = extractFeatures(roi1, C1);
-[features2, valid_points2] = extractFeatures(roi2, C2);
+ points1 = detectSURFFeatures(I1, 'NumOctaves', 6, 'NumScaleLevels', 10,'MetricThreshold', 500);
+ points2 = detectSURFFeatures(I2, 'NumOctaves', 6, 'NumScaleLevels', 10,'MetricThreshold', 500);
+ 
+[features1, valid_points1] = extractFeatures(I1,  points1);
+[features2, valid_points2] = extractFeatures(I2,  points2);
 
 indexPairs = matchFeatures(features1, features2);
 
 matchedPoints1 = valid_points1(indexPairs(:, 1), :);
 matchedPoints2 = valid_points2(indexPairs(:, 2), :);
 
-delta=mean(matchedPoints2-matchedPoints1)
+% Remove Outliers
+delta=(matchedPoints2.Location-matchedPoints1.Location);
+dist=sqrt(delta(:,1).*delta(:,1) + delta(:,2).*delta(:,2));
+
+mean_dist=mean(dist);
+stdev_dist=std(dist);
+
+count=1;
+for i=1:length(dist)
+    if (dist(i)<mean_dist+stdev_dist) && (dist(i)> mean_dist-stdev_dist)
+        filtered_Points1(count,:) = matchedPoints1.Location(i,:);
+        filtered_Points2(count,:) = matchedPoints2.Location(i,:);
+        count=count+1;
+    end
+end    
+
+%Debug
+% deltaa=(filtered_Points2-filtered_Points1);
+%dista=sqrt(deltaa(:,1).*deltaa(:,1) + deltaa(:,2).*deltaa(:,2));
+%delta1=round(mean(matchedPoints2.Location-matchedPoints1.Location));
+
+% Calculate the delta
+delta=round(mean(filtered_Points2 - filtered_Points1))
 toc
 
-figure; showMatchedFeatures(roi1, roi2, matchedPoints1, matchedPoints2);
+figure; showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
    
